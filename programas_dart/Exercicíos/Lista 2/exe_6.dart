678ec8067772cae28e080345dcdc9cda0ad5bd45
CarlosGardelG <cgga1@aluno@ifal.edu.br>
2026-03-02 20:43:36 -0300

    Exercício 6 e 7 da lista 2, Dart

--- programas_dart/Exercicíos/Lista 2/exe_6.dart
+++ programas_dart/Exercicíos/Lista 2/exe_6.dart
@@ -4,3 +4,63 @@
 3. Retornar à multiplicação entre dois números;
 4. Retornar a divisão entre dois números;
 Obs.: Cada opção deve ser uma função diferente.*/
+import 'dart:io';
+double soma(double num1, double num2){
+  return num1 + num2;
+}
+double subtracao (double num1, double num2){
+  return num1 - num2;
+}
+double multiplicacao (double num1, double num2){
+  return num1 * num2;
+}
+double divisao (double num1, double num2){
+  if (num2 != 0){
+    return num1 / num2;
+  }
+  else{
+    print("Erro: divisão por zero não é permitida.");
+    return double.infinity;
+      }
+}
+void main(){
+  print ("CALCULADORA");
+  print ("1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão");
+  print ("Selecione a opção de operação desejada:");
+  int ope = int.parse(stdin.readLineSync()!);
+  if (ope == 1){
+    print ("\nNúmero 1: ");
+    double num1 = double.parse(stdin.readLineSync()!);
+    print ("Número 2: ");
+    double num2 = double.parse(stdin.readLineSync()!);
+    
+    print ("O valor da soma entre $num1 e $num2 é: ${soma(num1, num2)}");
+  }
+  else if (ope == 2){
+    print ("\nNúmero 1: ");
+    double num1 = double.parse(stdin.readLineSync()!);
+    print ("Número 2: ");
+    double num2 = double.parse(stdin.readLineSync()!);
+    
+    print ("O valor da subtração entre $num1 e $num2 é: ${subtracao(num1, num2)}");
+  }
+  else if (ope == 3){
+    print ("\nNúmero 1: ");
+    double num1 = double.parse(stdin.readLineSync()!);
+    print ("Número 2: ");
+    double num2 = double.parse(stdin.readLineSync()!);
+    
+    print ("O valor da multiplicação entre $num1 e $num2 é: ${multiplicacao(num1, num2)}");
+  }
+  else if (ope == 4){
+    print ("\nNúmero 1: ");
+    double num1 = double.parse(stdin.readLineSync()!);
+    print ("Número 2: ");
+    double num2 = double.parse(stdin.readLineSync()!);
+    
+    print ("O valor da divisão entre $num1 e $num2 é: ${divisao(num1, num2)}");
+  }
+  else{
+    print("\nERR. Insira valor válido\n");
+  }
+}
--- programas_dart/Exercicíos/Lista 2/exe_7.dart
+++ programas_dart/Exercicíos/Lista 2/exe_7.dart
@@ -1,1 +1,48 @@
 //Questão 07: Escreva um programa com uma função que receba um vetor como parâmetro e calcule a média dos valores contidos no vetor. Obs.: O tamanho do vetor deve ser informado pelo usuário.
+import 'dart:io';
+void main(){
+  print("Escreva o tamanho do vetor para realizar a média aritmética: ");
+  int tamanho = int.parse(stdin.readLineSync()!);
+
+  List <double> listVal = [];
+  
+  for (int i = 0; i < tamanho; i++){
+    print("\nEscreva o valor na posição $i:");
+    /*
+    ///Primeira versão do código, feita com auxilío do Copilot, onde se faz desnecessário esse "if...else", já que o o código sempre retorna um valor diferente de nulo (por conta do "stdin.readLineSync()!"), então ele nunca receberá um valor nulo.
+    double numList = double.parse(stdin.readLineSync()!);
+    if (numList != null){
+      listVal.add(numList);
+    }
+    else{
+      print("Valor inválido. Coloque um número válido no lugar");
+      i--;
+    }*/
+    
+    /*///Aqui, o código sempre retorna um valor diferente de nulo (por conta do "stdin.readLineSync()!"). Assim sendo, pode-se diretamente adicionar na lista.
+    double numList = double.parse(stdin.readLineSync()!);
+    listVal.add(numList);*/
+
+    ///Aqui, o código pode retornar um valor nulo ou algo que não seja um número. Assim, o programa pede para o usuário colocar um número válido no lugar, e o "i--" faz com que o programa volte para a posição anterior, ou seja, ele não avança para a próxima posição do vetor até que o usuário coloque um valor válido.
+    double? numList = double.tryParse(stdin.readLineSync()!);
+    if (numList != null){
+      listVal.add(numList);
+    } 
+    else {
+      print("Valor inválido. Coloque um número válido no lugar");
+      i--;
+    }
+    
+  }
+  
+  print("\n\nResultado da média aritmética: ${calcularMedia(listVal)}");
+}
+ 
+double calcularMedia(List <double> vetor){
+  double valor = 0;
+  for (int i = 0; i < vetor.length; i++){
+    valor += vetor[i];
+  }
+  return valor / vetor.length;
+}
+
