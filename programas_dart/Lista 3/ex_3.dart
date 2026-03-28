ed3dfb446c9aedfb0fd650b302f31e11c630d805
CarlosGardelG <cgga1@aluno@ifal.edu.br>
2026-03-28 02:09:28 -0300

    Ajeitando pra fazer tudo direito depois

--- programas_dart/Lista 3/ex_3.dart
+++ programas_dart/Lista 3/ex_3.dart
@@ -3,29 +3,3 @@
 2. Listar livros por editora
 3. Listar livros por autor
 4. Listar todos os livros disponíveis*/
-import 'dart:io';
-class Livro {
-  String titulo;
-  int isbn;
-  String editora;
-  String categoria;
-  String anoEdicao;
-
-  Livro (this.titulo, this.isbn, this.editora, this.categoria, this.anoEdicao);
-}
-void main(){
-  List <Livro> livros = [];
-  int opc = 0;
-  while (opc == 0){
-    
-
-
-
-
-
-    opc = int.parse(stdin.readLineSync()!);
-  }
-}
-
-//[] - Iniciar o código com um for de 5 livros 
-  //[] - Fazer uma lista númerica (1., 2., 3.) para tipos de categoria, para settar dentro da variavel categoria na classe
--- programas_dart/Lista 3/ex_4.dart
+++ programas_dart/Lista 3/ex_4.dart
@@ -3,16 +3,3 @@
 2. Mostrar as pessoas que estão dentro do peso ideal (IMC > 18,5 e IMC < 25)
 3. Mostrar as pessoas que estão acima do peso (IMC > = 25)
 Obs.: imc = peso/(altura ∗ altura)*/
-import 'dart:io';
-class IMC {
-  String _nome;
-  double _peso, _altura;
-
-  IMC (this.peso, this.altura);
-
-  double calcIMC() => _peso / (_altura * _altura);
-}
-void main(){
-  IMC imc = IMC;
-  for 
-}
